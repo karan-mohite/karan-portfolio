eda426f7a61cef95fad44b5b87ba347a9ff31faa
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-08 19:53:05 +0530

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -40,7 +40,7 @@ export default function Hero() {
 
                   <Button asChild variant="outline" className="bg-transparent text-blue-600 dark:text-blue-400 border-blue-600 dark:border-blue-400 hover:bg-blue-600 dark:hover:bg-blue-400 hover:text-white">
                   <a href="mailto:mohitekaran18@gmail.com" target="_blank" rel="noopener noreferrer">
-                    <svg viewBox="0 0 24 24" className="mr-2 h-4 w-4">
+                    <svg className="mr-2 h-4 w-4" fill="currentColor">
                       <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
                     </svg>
                     Email
