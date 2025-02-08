686b55e74d4bd7ad0c8e6b944d34d56099e4eb10
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-08 19:56:36 +0530

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -53,6 +53,17 @@ export default function Hero() {
                       <Mail className="mr-2 h-4 w-4" />Resume
                     </a>
                   </Button>
+                  <br></br>
+                  
+                  <Button asChild variant="outline" className="bg-transparent text-blue-600 dark:text-blue-400 border-blue-600 dark:border-blue-400 hover:bg-blue-600 dark:hover:bg-blue-400 hover:text-white">
+                <a href="mailto:mohitekaran18@gmail.com" target="_blank" rel="noopener noreferrer">
+                  <svg viewBox="0 0 24 24" className="mr-2 h-4 w-4" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+                  </svg>
+                  Email
+                </a>
+              </Button>
+
 
                   <br></br>
 
